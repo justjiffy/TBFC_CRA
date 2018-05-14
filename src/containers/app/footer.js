c6e139f4b41c10b275dd2110db4d2ed65c87a4f7
Your Name <justjiffy@gmail.com>
2018-05-13 20:20:59 -0700

    redux navigation toggle w toggle working on window resize!

---
+++ src/containers/app/footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    Topanga Banjo&middot;Fiddle Contest and Folk Festival, Inc.
+  </footer>
+)
+
+export default Footer;
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 import counter from './counter'
+import nav from './nav'
 
 export default combineReducers({
   routing: routerReducer,
-  counter
+  counter,
+  nav
 })
